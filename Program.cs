095f540488fc4c9a49eed1e73261602945c78625
vmvpro <melnvetal@gmail.com>
2019-08-23 00:10:09 +0300

    Создание класса Resource

--- Program.cs
+++ Program.cs
@@ -375,47 +375,6 @@ namespace ExcelReadC
 
         public static Dictionary<string, string> KSM = new Dictionary<string, string>();
 
-		static void  GroupByListRecource()
-		{
-			string old_kmat1 = "00123";
-			string old_kmat2 = "00123";
-			string old_kmat3 = "00222";
-			string old_kmat4 = "00333";
-			string old_kmat5 = "00333";
-
-			var list = new List<string>() { "00123", "00123", "00222", "00333", "00333" };
-			var listGroupBy = list.GroupBy(x => x);
-
-			Dictionary<string, int> dic = new Dictionary<string, int>();
-
-			foreach (var grp in listGroupBy)
-			{
-				dic.Add(grp.Key, grp.Count());
-			}
-
-			int key = listGroupBy.Where(x => x.Key == "00123").Count();
-
-			//Dictionary<string, > dic_ = listGroupBy.ToDictionary();
-
-
-
-			var listGroups =
-				from prod in list
-				group prod by list into prodGroup
-				select new { OldKmat = prodGroup.Key, Count = prodGroup.Count() };
-
-
-			//Dictionary<string, int> dic = new Dictionary<string, int>();
-
-			//foreach (var item in listGroups)
-			//	Console.WriteLine(item.OldKmat + " - " + item.Count);
-			//    dic.Add(item.OldKmat, item.Count);
-
-
-
-			//var where = listGroupBy.Where((x,y) => x == y);
-		}
-
         static void Main2(string path, string fileName)
         {
             //string path = "Data.xlsx";
